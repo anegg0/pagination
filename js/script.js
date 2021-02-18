f6ad345abe17511eb7a298797b207cf27c3ccb4e
Gael Blanchemain <anegg0@pop-os.localdomain>
2021-02-18 13:11:40 -0800

    students display:ok

--- js/script.js
+++ js/script.js
@@ -23,20 +23,38 @@ function showPage(list, page) {
    // const endIndex = page parameter * items per page
    const endIndex = page  * 9;
   // select the element with a class of `student-list` and assign it to a variable
-   let studentsUl = document.querySelector('.student-list');
-   console.log(studentsUl);
-   let li = document.createElement('li');
-   li.classList.add('.student-item')
-   studentsUl.appendChild(li);
-   let divStudents = document.createElement('student-details');
   // set the innerHTML property of the variable you just created to an empty string
    studentList = '';
 
-      //  const ul = document.querySelector('ul .student-list');
-      // console.log(list.length);
   // loop over the length of the `list` parameter
-//   for (let i = 0; i < list.length; i++) {
-//     if (i >= startIndex && i <= endIndex -1) {
+  for (let i = 0; i < list.length; i++) {
+    if (i >= startIndex && i <= endIndex -1) {
+   let studentsUl = document.querySelector('.student-list');
+   console.log(studentsUl);
+   let li = document.createElement('li');
+   li.classList.add('student-item');
+   studentsUl.appendChild(li);
+   let divStudents = document.createElement('div');
+   divStudents.classList.add('student-details');
+   li.appendChild(divStudents);
+   let img = document.createElement('img');
+   img.classList.add('avatar');
+   img.src=list[i].picture.thumbnail;
+   divStudents.appendChild(img);
+   let h3 = document.createElement('h3');
+   divStudents.appendChild(h3);
+   h3.innerText = `${list[i].name.first} ${list[i].name.last}`;
+   let spanEmail = document.createElement('span');
+   spanEmail.classList.add('email');
+   spanEmail.innerText = list[i].email;
+   divStudents.appendChild(spanEmail);
+   let divDetails = document.createElement('div');
+   divDetails.classList.add('joined-details');
+   li.appendChild(divDetails);
+   let spanDate = document.createElement('span');
+   spanDate.classList.add('date');
+   spanDate.innerText = list[i].registered.date;
+   divDetails.appendChild(spanDate);
 // li = document.createElement('li');
       //  const h3 = document.querySelector('h3');
       //  const divStudentDetails = document.querySelector('div #student-details');
@@ -44,12 +62,12 @@ function showPage(list, page) {
 
       // console.log(list[i].name.last) 
     } 
-//   }
+  }
     // inside the loop create a conditional to display the proper students
       // inside the conditional:
         // create the elements needed to display the student information
         // insert the above elements 
-// }
+}
 showPage(data, 2);
 
 /*
